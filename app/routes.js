0fda32b47a915fccce77f03e6768167b430220a7
fiseraj23 <fiseraj23@spstrutnov.cz>
2026-05-06 12:45:24 +0200

    Routing

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,12 @@
-import { index } from "@react-router/dev/routes";
+import { index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.jsx")];
+export default [
+  // Cesta: / (Index, Domů - seznam událostí)
+  index("./index.jsx"),
+  
+  // Cesta: /events/new (Přidat událost)
+  route("events/new", "./new-event.jsx"),
+
+  // Cesta: /events/:id (Detail události, :id je dynamický parametr)
+  route("events/:id", "./event-detail.jsx")
+];
