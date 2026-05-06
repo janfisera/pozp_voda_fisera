82e218d11865194376957aaccc04b9bbd3b03980
fiseraj23 <fiseraj23@spstrutnov.cz>
2026-05-06 13:19:26 +0200

    NEw Events START

---
+++ app/routes/new-event.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/header";
+
+
+export default function NewEvent() {
+  return (
+    <>
+        <Header />
+        <article class="bg-gray-200">
+            <h1 class="text-2xl text-center">Nová událost</h1>
+        </article>
+    </>
+  );
+}
